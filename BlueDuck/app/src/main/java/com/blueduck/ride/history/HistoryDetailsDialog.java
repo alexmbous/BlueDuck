cee939dfe25309e024e5e87b3a61d65642f64b8b
Garrett Sangalli <gsharp.impact@gmail.com>
2019-03-21 13:13:44 -0500

    ride amount to two decimal places fixes

--- BlueDuck/app/src/main/java/com/blueduck/ride/history/HistoryDetailsDialog.java
+++ BlueDuck/app/src/main/java/com/blueduck/ride/history/HistoryDetailsDialog.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.blueduck.ride.R;
 import com.blueduck.ride.history.bean.HistoryBean;
+import com.blueduck.ride.utils.CurrencyUtil;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.gms.maps.model.LatLng;
@@ -97,9 +98,7 @@ public class HistoryDetailsDialog extends Dialog implements View.OnClickListener
         rideTime.setText(Utils.hourMinuteFormat(sumTime));
         if (historyBean.getAmount() != null) {
             double amount = Double.parseDouble(historyBean.getAmount());
-            DecimalFormat twoPlaces = new DecimalFormat("0.00");
-            String fixedAmount = twoPlaces.format(amount);
-            cost.setText(mContext.getString(R.string.dollar) + fixedAmount);
+            cost.setText(mContext.getString(R.string.dollar) + CurrencyUtil.convertToTwoDecimalPlaces(amount));
         }else{
             cost.setText(mContext.getString(R.string.dollar) + 0.0);
         }
